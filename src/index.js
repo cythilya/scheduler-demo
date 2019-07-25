7ee2a28487dfc46c58a06307b9307afaae5e9f85
summer_tang <cythilya@gmail.com>
2019-07-25 18:30:51 +0800

    schedular package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {
+  unstable_runWithPriority,
+  unstable_scheduleCallback,
+  unstable_LowPriority
+} from 'scheduler';
+
 import './styles.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { count: 0, value: '' };
+    this.state = { count: 0 };
     this.setCount = this.setCount.bind(this);
   }
 
@@ -14,22 +20,31 @@ class App extends Component {
     const { count } = this.state;
     const newCount = count + 1;
     this.setState({ count: newCount });
+
+    this.doSecond();
+    this.doFirst();
+  }
+
+  doFirst() {
+    unstable_scheduleCallback(() => {
+      console.log('first');
+    });
+  }
+
+  doSecond() {
+    unstable_runWithPriority(unstable_LowPriority, () => {
+      unstable_scheduleCallback(() => {
+        console.log('secoed'); // low priority: 4
+      });
+    });
   }
 
   render() {
-    const { sayHi } = this.props;
     const { count } = this.state;
 
     return (
       <div className="App">
-        <div>{sayHi}</div>
         <div>{count}</div>
-        <input
-          value={this.state.value}
-          onChange={(e) => {
-            this.setState({value: e.target.value});
-          }}
-        />
         <button onClick={this.setCount}>Click me! {count}</button>
       </div>
     );
